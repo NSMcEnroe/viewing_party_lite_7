4c5fd5140ca14103a37508b8a1ee9112e2b2be43
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-10-10 17:09:47 -0600

    test: Test User, ViewingParty, UserViewingParty models and validations

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,2 +1,7 @@
 class ViewingParty < ApplicationRecord
+  has_many :user_viewing_parties
+  has_many :users, through: :user_viewing_parties
+
+  validates :start_time, presence: true  
+  validates :duration, presence: true, numericality: true 
 end
