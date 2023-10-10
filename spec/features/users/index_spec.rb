8dbbd0c629c6e75013a3da58fa1ddffeab05c24c
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-10-10 17:19:39 -0600

    test: Start User Index Page spec

---
+++ spec/features/users/index_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "User Index Page", type: :feature do
+  it "displays the title of appliction" do
+
+  end
+
+  it "displays a button to create a new user" do
+
+  end
+
+  it "displays a list of existing users which links to the users dashboard" do
+
+  end
+
+  it "displays a link to go back to the landing page" do
+
+  end
+end
