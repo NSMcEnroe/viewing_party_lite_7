ba0f4c8561dd91838edd73ae46a27f1c655f796c
Nicholas McEnroe <nsmcenro@gmail.com>
2023-10-11 10:31:14 -0600

    Feat: Add register page to the app and form to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
 
-    @user.save
+    user.save
     # if !merchant.save
     #   flash.now[:alert] = "Please Input a Name"
     #   render :new
     # else
-    redirect_to "/users/#{@user.id}"
+    redirect_to "/users/#{user.id}"
   end
 
   private
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -6,21 +6,20 @@ RSpec.describe 'User New Page', type: :feature do
   it 'displays a form that the user can fill in and submit' do
     visit '/register'
 
-    fill_in('user[name]', with: 'Peggy Sue')
-    fill_in('user[email]', with: 'psue@turing.edu')
+    fill_in('name', with: 'Peggy Sue')
+    fill_in('email', with: 'psue@turing.edu')
 
     #Refactor Later
 
     click_button('Register')
 
-    new_user = User.last
+    user = User.last
 
-    require 'pry';binding.pry
     expect(page).to have_content('Viewing Party')
 
-    expect(current_path).to eq("/users/#{new_user.id}")
-    expect(new_user.name).to eq('Peggy Sue')
-    expect(new_user.email).to eq('psue@turing.edu')
+    expect(current_path).to eq("/users/#{user.id}")
+    expect(user.name).to eq('Peggy Sue')
+    expect(user.email).to eq('psue@turing.edu')
   end
 
   xit 'denies a new user if the email is not unique' do
